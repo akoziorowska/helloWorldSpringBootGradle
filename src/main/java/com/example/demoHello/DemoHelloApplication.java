716bf21c8f0a8870aae3f185f4d59150ea70d6fb
akoziorowska <akoziorowska2001@gmail.com>
2018-09-13 20:54:50 +0200

    for non-web application

--- src/main/java/com/example/demoHello/DemoHelloApplication.java
+++ src/main/java/com/example/demoHello/DemoHelloApplication.java
@@ -1,13 +1,18 @@
 package com.example.demoHello;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoHelloApplication {
+public class DemoHelloApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		//SpringApplication.run(DemoHelloApplication.class, args);
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(DemoHelloApplication.class, args);
+	}
+
+	@Override
+	public void run (final String... strings)throws Exception{
 		System.out.println("Hello world");
 	}
 }
